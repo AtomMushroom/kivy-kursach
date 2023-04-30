251a64b88566a5621baf4f589caf290d552ad02a
AtomMushroom <vasilyev.atom@gmail.com>
2023-04-30 19:25:39 +0300

    main files

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from kivymd.app import MDApp
+
+from kivy.lang import Builder
+Builder.load_file('Convert.kv')
+
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+from mainScreen import MainScreen
+from basicScreen import BasicScreen
+from aboutScreen import AboutScreen
 
 Window.size = (480, 853) #Размер окна, в 2,25 раз меньше, чем 1080*1920 
 
@@ -8,13 +16,17 @@ class ConvertApp(MDApp): #Базовый класс
 	def build(self):
 		self.title = "Нестандартный конвертер чисел"
 		self.sm = ScreenManager()
-		self.sm.add_widget(MainScreen(name='main'))
+		self.mainScreen = MainScreen(name='main')
+		self.basicScreen = BasicScreen(name='basic')
+		self.aboutScreen = AboutScreen(name='about')
+
+		self.sm.add_widget(self.mainScreen)
+		self.sm.add_widget(self.basicScreen)
+		self.sm.add_widget(self.aboutScreen)
+
 		self.theme_cls.theme_style = 'Dark'
 
 		return self.sm
-
-class MainScreen(Screen):
-	pass
 
 if __name__ == '__main__':
     ConvertApp().run()
