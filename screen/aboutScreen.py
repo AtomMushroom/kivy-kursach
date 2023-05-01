bb501decbc30e56482cd2ec3a0a48d19b62d0d8f
AtomMushroom <vasilyev.atom@gmail.com>
2023-05-01 10:21:12 +0300

    Compilation

--- screen/aboutScreen.py
+++ screen/aboutScreen.py
@@ -1,6 +1,5 @@
 import kivy
 from kivymd.uix.screen import MDScreen
-import webbrowser
 
 class AboutScreen(MDScreen):
 	"""
@@ -26,14 +25,3 @@ class AboutScreen(MDScreen):
 		"""
 		self.ids.nav_drawer.set_state("close")
 		self.manager.current = 'about'
-
-	def openSite(self, index):
-		"""
-		Открыть в браузере веб ресурсы организаций
-		"""
-		if index == 1:
-			webbrowser.open("https://www.python.org/")
-		elif index == 2:
-			webbrowser.open("https://kivy.org/")
-		else:
-			webbrowser.open("https://kivymd.readthedocs.io/en/1.1.1/")
